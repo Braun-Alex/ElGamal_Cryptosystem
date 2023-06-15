56299f3ce247a2d7aaf286ee99f63d8364f67de3
Braun_Alex <lamanasakasa@gmail.com>
2023-06-15 05:06:55 +0300

    Unit tests and displaying in cmd for ElGamal signature have been added

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,21 @@ import (
 )
 
 func main() {
-	message := "Hi"
+	message := "ZK-STARK has big impact on StarkNet"
 	privateKey, publicKey := keypair.GenerateKeypair()
+	fmt.Print("*********** ElGamal cryptosystem ***********\n")
+	defer fmt.Printf("Private key: %d\n", privateKey)
+	defer fmt.Printf("Public key: %d\n", publicKey)
+	fmt.Print("Message: \"", message, "\"\n")
 	r, s := signature.Sign(message, privateKey)
 	isValidSignature := signature.Verify(message, publicKey, r, s)
+	fmt.Print("********* ElGamal signing *********\n")
+	defer fmt.Printf("Parameter R: %d\n", r)
+	defer fmt.Printf("Parameter S: %s\n", s)
+	fmt.Print("Signature is valid: ")
 	if isValidSignature {
-		fmt.Print("Signature is valid")
+		fmt.Print("yes\n")
 	} else {
-		fmt.Print("Signature is not valid")
+		fmt.Print("no\n")
 	}
 }
--- test/signature_test.go
+++ test/signature_test.go
@@ -1,14 +1,64 @@
 package test
 
 import (
+	"ElGamal_Cryptosystem/pkg/keypair"
 	"ElGamal_Cryptosystem/pkg/signature"
+	"math/big"
 	"testing"
 )
 
-func TestPlusOperation(test *testing.T) {
-	expectedResult := 5 + 3
-	actualResult := signature.Plus(5, 3)
-	if expectedResult != actualResult {
-		test.Errorf("Некоректна сума чисел 5 і 3: %d", actualResult)
+func TestCorrectSignature(test *testing.T) {
+	message := "ZK-STARK has big impact on StarkNet"
+	privateKey, publicKey := keypair.GenerateKeypair()
+	r, s := signature.Sign(message, privateKey)
+	isValidSignature := signature.Verify(message, publicKey, r, s)
+	if !isValidSignature {
+		test.Errorf("One does not accept correct ElGamal signature")
+	}
+}
+
+func TestIncorrectSignatureOnDifferentData(test *testing.T) {
+	message := "ZK-STARK has big impact on StarkNet"
+	anotherMessage := "LayerZero is cross-chain protocol"
+	privateKey, publicKey := keypair.GenerateKeypair()
+	r, s := signature.Sign(message, privateKey)
+	isValidSignature := signature.Verify(anotherMessage, publicKey, r, s)
+	if isValidSignature {
+		test.Errorf("One accepts incorrect ElGamal signature on different data")
+	}
+}
+
+func TestIncorrectSignatureOnDifferentKeys(test *testing.T) {
+	message := "ZK-STARK has big impact on StarkNet"
+	privateKey, _ := keypair.GenerateKeypair()
+	_, anotherPublicKey := keypair.GenerateKeypair()
+	r, s := signature.Sign(message, privateKey)
+	isValidSignature := signature.Verify(message, anotherPublicKey, r, s)
+	if isValidSignature {
+		test.Errorf("One accepts incorrect ElGamal signature on another public key")
+	}
+}
+
+func TestIncorrectSignatureOnIncorrectParameterR(test *testing.T) {
+	message := "ZK-STARK has big impact on StarkNet"
+	privateKey, _ := keypair.GenerateKeypair()
+	_, anotherPublicKey := keypair.GenerateKeypair()
+	r, s := signature.Sign(message, privateKey)
+	r.Add(r, big.NewInt(3))
+	isValidSignature := signature.Verify(message, anotherPublicKey, r, s)
+	if isValidSignature {
+		test.Errorf("One accepts incorrect ElGamal signature on changed parameter r")
+	}
+}
+
+func TestIncorrectSignatureOnIncorrectParameterS(test *testing.T) {
+	message := "ZK-STARK has big impact on StarkNet"
+	privateKey, _ := keypair.GenerateKeypair()
+	_, anotherPublicKey := keypair.GenerateKeypair()
+	r, s := signature.Sign(message, privateKey)
+	s.Add(s, big.NewInt(3))
+	isValidSignature := signature.Verify(message, anotherPublicKey, r, s)
+	if isValidSignature {
+		test.Errorf("One accepts incorrect ElGamal signature on changed parameter s")
 	}
 }
